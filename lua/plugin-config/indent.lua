acbab96b579bd588213453bfabc3af0a0ca5326a
3000ye <hcg2924239214@163.com>
2023-01-17 22:15:01 +0800

    增加了彩虹缩进插件

--- lua/plugin-config/indent.lua
+++ lua/plugin-config/indent.lua
@@ -1,23 +1,35 @@
 vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent1 guibg=#8fbcbb gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent2 guibg=#88c0d0 gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent3 guibg=#bf616a gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent4 guibg=#d08770 gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent5 guibg=#ebcb8b gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent6 guibg=#a3be8c gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent7 guibg=#b48ead gui=nocombine]]
+
+vim.cmd [[highlight indentblanklineindent1 guibg=#8fbcbb gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent2 guibg=#88c0d0 gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent3 guibg=#bf616a gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent4 guibg=#d08770 gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent5 guibg=#ebcb8b gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent6 guibg=#a3be8c gui=nocombine]]
+vim.cmd [[highlight indentblanklineindent7 guibg=#b48ead gui=nocombine]]
 
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "space: "
 
 require("indent_blankline").setup {
-    space_char_blankline = " ",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
+  indent_blankline_char = " ",
+  indnet_blankline_char_blankline = " ",
+  show_end_of_line = true,
+
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
+    "IndentBlanklineIndent7",
+  },
 }
